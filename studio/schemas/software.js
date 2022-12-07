f672073db15f8c167a528926e4ae379345a93e0c
Fredrikke Maria Kulsrud <fmkulsrud@gmail.com>
2022-12-07 09:14:32 +0100

    Add slug to software schema

--- studio/schemas/software.js
+++ studio/schemas/software.js
@@ -7,6 +7,14 @@ export default {
             name: 'software',
             title: 'Software',
             type: 'string',
+        },
+        {
+            name: 'slug',
+            tilte: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'software'
+            }
         }
     ]
 }
