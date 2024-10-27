4ebc3bea63baa02848e40a2c8a8407ea26c15861
Fredrikke Maria Kulsrud <fmkulsrud@gmail.com>
2024-10-27 21:12:25 +0100

    Cv changes

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -1,15 +1,18 @@
-export default function handleHambuger() {
+export default function handleHamburger() {
   const hamburgerIconEl = document.querySelector(".mobile-menu-icon");
-  const overlayMenuEl = document.querySelector(".overlay-menu");
+  const overlayMenuEl = document.getElementById("mobile-menu");
   const exitMenuEl = document.querySelector(".close");
 
-  /* e er en naturlig oppførsel, men å bruke prevent så nullstiller vi.*/
   hamburgerIconEl.addEventListener("click", (e) => {
     e.preventDefault();
     overlayMenuEl.classList.remove("hidden");
   });
+
   exitMenuEl.addEventListener("click", (e) => {
     e.preventDefault();
     overlayMenuEl.classList.add("hidden");
   });
 }
+
+// Call the function to initialize the event listeners
+handleHamburger();
